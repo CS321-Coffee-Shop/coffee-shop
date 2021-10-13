749a0e4a56b14e2d8a990b02d404993a1dd7cfed
mungbeansss <56747708+mungbeansss@users.noreply.github.com>
2021-10-13 17:12:04 -0400

    Update CoffeeShop.java

--- CoffeeShop.java
+++ CoffeeShop.java
@@ -6,28 +6,38 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class CoffeeShop{
-   public CoffeeShop(){
+
+   public void login(){
+   
+   }
+   public void openProgram(){
       
    }
-   public void OpenProgram(){
-      
+   public void readInMenu(){
+   
    }
    public static void main(String[] args){
+   
       Scanner input = new Scanner(System.in);
       JFrame frame = new JFrame("Coffee Shop");
       frame.setLayout(new GridLayout(1,1));
       frame.setLocationRelativeTo(null);
-      frame.setPreferredSize(new Dimension(600,400));
+      frame.setPreferredSize(new Dimension(800,600));
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      JPanel panel = new JPanel();
+      
+      JButton menuButton = new JButton("Menu");
+      JButton preferButton = new JButton("Input Your Preferences");
+      JButton randomButton = new JButton("Randomized Drink");
+      
+      frame.getContentPane().add(panel);
+      panel.add(menuButton);
+      panel.add(preferButton);
+      panel.add(randomButton);
+      
       frame.pack();         
       frame.setVisible(true);
-      
-      JTabbedPane pane = new JTabbedPane();
-      JPanel panel = new JPanel();
-      /*
-      pane.addTab("Kitchen", new KitchenPanel());
-      frame.getContentPane().add(pane);
-      frame.setVisible(true);
-      */
+            
    }   
 }
