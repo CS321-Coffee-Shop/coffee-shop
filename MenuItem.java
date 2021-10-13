4e42d3449a28767c88a92dacee23f5e4c369cf8c
mungbeansss <56747708+mungbeansss@users.noreply.github.com>
2021-10-13 14:07:23 -0400

    Update MenuItem.java

--- MenuItem.java
+++ MenuItem.java
@@ -1,9 +1,23 @@
 public class MenuItem{
    
+   String itemName;
+   double price;
+   String[] ingredients;
+   
    public MenuItem(){
-      
+      this.itemName = "";
+      this.price = 0.0;
+      this.ingredients = new String[];   
+   }
+   public MenuItem(String itemName, double price, String[] ingredients){
+      this.itemName = itemName;
+      this.price = price;
+      this.ingredients = ingredients;
+   }   
+   public void setItemIngredients(){
       
    }
-   
-   
+   public String[] getItemIngredients(){
+      
+   }
 }
