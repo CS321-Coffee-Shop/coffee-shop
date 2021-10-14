0f9d3c8c0818dcafeeb17cb5b8ee3c6c49f30517
tbourge <78454075+tbourge@users.noreply.github.com>
2021-10-14 19:10:47 -0400

    Add string password and associated methods

--- Customer.java
+++ Customer.java
@@ -4,19 +4,21 @@ public class Customer{
    String lastName;
    String phoneNumber;
    String email;
-   
+   String password;
  
    public Customer(){
       firstName = "";
       lastName = "";
       phoneNumber = "";
       email = "";
+      password = "";
    }
-   public Customer(String firstName, String lastName, String phoneNumber, String email){
+   public Customer(String firstName, String lastName, String phoneNumber, String email, String password){
       this.firstName = firstName;
       this.lastName = lastName;
       this.phoneNumber = phoneNumber;
       this.email = email;
+      this.password = password;
    }
    public String getName(){
       return firstName+" "+lastName;
@@ -31,4 +33,11 @@ public class Customer{
    public void setPhoneNumber(String phoneNumber){
       this.phoneNumber = phoneNumber;
    }
+   public String getPassword(){
+      return this.password;
+   }
+   public void setPassword(String password){
+      this.password = password;
+   }
+
 }
